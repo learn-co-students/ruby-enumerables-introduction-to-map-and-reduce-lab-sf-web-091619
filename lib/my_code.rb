7edb0862f12ed1815d6603259da198ca58583574
Adam La Rosa <adam.larosa@gmail.com>
2019-09-06 18:52:15 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -42,6 +42,36 @@ def map_to_square(source_array)
 end
 
 
-def reduce_to_total(source_array, starting_point)
-  
+def reduce_to_total(source_array, starting_point=0)
+  count = 0
+  total = starting_point
+  while source_array[count] do
+    total = total + source_array[count]
+    count += 1
+  end
+  return total 
+end
+
+
+def reduce_to_all_true(source_array)
+  count = 0
+  while count < source_array.size do
+    if source_array[count] == false then 
+      return false 
+    end
+    count += 1
+  end
+  return true
+end
+
+
+def reduce_to_any_true(source_array)
+  count = 0
+  while count < source_array.size
+    if source_array[count] == true then
+      return true
+    end
+    count += 1
+  end
+  return false
 end
